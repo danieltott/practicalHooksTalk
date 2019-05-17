f1ba42f535bc598248af1aac28affb0a31aac363
Dan Ott <dan@dtott.com>
2019-05-17 15:50:57 -0400

    use hooks for filter

--- src/Filter.js
+++ src/Filter.js
@@ -1,115 +1,135 @@
-import React from 'react';
+import React, { useReducer, useEffect } from 'react';
 import SelectUsers from './SelectUsers';
 
-export default class Filter extends React.Component {
-  constructor(props) {
-    super(props);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_FORM_INPUT':
+      const formData = {
+        ...state.formData,
+        [action.name]: action.value
+      };
 
-    this.updateFormInput = this.updateFormInput.bind(this);
+      return {
+        ...state,
+        formData,
+        isValid: false
+      };
+    case 'VALIDATE':
+      return {
+        ...state,
+        isValid: action.isValid
+      };
+    default:
+      throw new Error('Invalid action type');
   }
+};
 
-  state = {
-    userId: '',
-    showCompleted: 'All',
+const Filter = ({
+  isLoadingUsers,
+  users,
+  updateUserSettings,
+  refreshUsers
+}) => {
+  const [state, dispatch] = useReducer(reducer, {
+    formData: {
+      userId: '',
+      showCompleted: 'All'
+    },
     isValid: false
-  };
+  });
 
-  updateFormInput(e) {
-    this.setState(
-      {
-        [e.currentTarget.name]: e.currentTarget.value
-      },
-      () => {
-        this.validate();
-      }
-    );
-  }
+  useEffect(() => {
+    const validate = async () => {
+      console.log('validating...');
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      dispatch({
+        type: 'VALIDATE',
+        isValid: !!(state.formData.userId && state.formData.showCompleted)
+      });
+    };
+    validate();
+  }, [state.formData]);
 
-  validate() {
-    this.setState({
-      isValid: !!(this.state.userId && this.state.showCompleted)
+  function updateFormInput(e) {
+    dispatch({
+      type: 'UPDATE_FORM_INPUT',
+      name: e.currentTarget.name,
+      value: e.currentTarget.value
     });
   }
 
-  render() {
-    const {
-      isLoadingUsers,
-      users,
-      updateUserSettings,
-      refreshUsers
-    } = this.props;
-    const { userId, showCompleted } = this.state;
-    return (
-      <form
-        data-testid="filtersform"
-        onSubmit={e => {
-          e.preventDefault();
-          updateUserSettings({ userId, showCompleted });
-        }}
-      >
-        <legend className="title is-6 is-spaced">Filter Todos:</legend>
-        <div className="field">
-          <label className="label">User:</label>
-          <div className="control">
-            <div className="select">
-              <SelectUsers
-                isLoading={isLoadingUsers}
-                users={users}
-                userId={this.state.userId}
-                onChange={this.updateFormInput}
-              />
-            </div>
+  return (
+    <form
+      data-testid="filtersform"
+      onSubmit={e => {
+        e.preventDefault();
+        updateUserSettings(state.formData);
+      }}
+    >
+      <legend className="title is-6 is-spaced">Filter Todos:</legend>
+      <div className="field">
+        <label className="label">User:</label>
+        <div className="control">
+          <div className="select">
+            <SelectUsers
+              isLoading={isLoadingUsers}
+              users={users}
+              userId={state.formData.userId}
+              onChange={updateFormInput}
+            />
           </div>
         </div>
-        <div>
-          <button onClick={refreshUsers} type="button">
-            Refresh Users...
-          </button>
+      </div>
+      <div>
+        <button onClick={refreshUsers} type="button">
+          Refresh Users...
+        </button>
+      </div>
+      <div className="field">
+        <div className="control">
+          <label className="radio">
+            <input
+              type="radio"
+              name="showCompleted"
+              value="All"
+              checked={state.formData.showCompleted === 'All'}
+              onChange={updateFormInput}
+            />{' '}
+            All
+          </label>
+          <label className="radio">
+            <input
+              type="radio"
+              name="showCompleted"
+              checked={state.formData.showCompleted === 'Completed'}
+              value="Completed"
+              onChange={updateFormInput}
+            />{' '}
+            Completed
+          </label>
+          <label className="radio">
+            <input
+              type="radio"
+              name="showCompleted"
+              checked={state.formData.showCompleted === 'Not Completed'}
+              value="Not Completed"
+              onChange={updateFormInput}
+            />{' '}
+            Not Completed
+          </label>
         </div>
-        <div className="field">
-          <div className="control">
-            <label className="radio">
-              <input
-                type="radio"
-                name="showCompleted"
-                value="All"
-                checked={this.state.showCompleted === 'All'}
-                onChange={this.updateFormInput}
-              />{' '}
-              All
-            </label>
-            <label className="radio">
-              <input
-                type="radio"
-                name="showCompleted"
-                checked={this.state.showCompleted === 'Completed'}
-                value="Completed"
-                onChange={this.updateFormInput}
-              />{' '}
-              Completed
-            </label>
-            <label className="radio">
-              <input
-                type="radio"
-                name="showCompleted"
-                checked={this.state.showCompleted === 'Not Completed'}
-                value="Not Completed"
-                onChange={this.updateFormInput}
-              />{' '}
-              Not Completed
-            </label>
-          </div>
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="button is-primary"
-            disabled={isLoadingUsers || !this.state.isValid}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    );
-  }
-}
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="button is-primary"
+          disabled={isLoadingUsers || !state.isValid}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Filter;
