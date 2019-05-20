f9e20badf0a94ef86d5da904b221ee6b9bc1c8b1
Dan Ott <dan@dtott.com>
2019-05-20 10:28:48 -0400

    use custom hook to finalize app

--- src/App.js
+++ src/App.js
@@ -1,157 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Api from './Api';
 import Todos from './Todos';
 import Filter from './Filter';
+import useLoadData from './useLoadData';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+const withErrorBoundary = WrappedComponent => {
+  return class extends React.Component {
+    state = {
+      error: null
+    };
+    componentDidCatch(error, info) {
+      console.log({ error, info });
+      this.setState({ error });
+    }
 
-    this.onFilterSubmit = this.onFilterSubmit.bind(this);
-    this.fetchTodos = this.fetchTodos.bind(this);
-    this.fetchUsers = this.fetchUsers.bind(this);
-    this.refreshUsers = this.refreshUsers.bind(this);
-  }
+    render() {
+      if (this.state.error) {
+        return (
+          <div>
+            <h3>There was an error:</h3>
+            <p>{this.state.error.message}</p>
+          </div>
+        );
+      }
+      return <WrappedComponent />;
+    }
+  };
+};
 
-  state = {
-    // fetching users state
-    users: null,
-    isLoadingUsers: false,
-    error: null,
-
-    // fetching todos state
-    todos: null,
-    isLoadingTodos: false,
-    todosUpdatedAt: null,
-    todosError: null,
-
-    // global
+const App = () => {
+  const [filterData, setFilterData] = useState({
     selectedUser: null,
     showCompleted: ''
+  });
+  const [usersState, loadUsers] = useLoadData(Api.fetchUsers);
+
+  const onFilterSubmit = formData => {
+    setFilterData({
+      selectedUser: usersState.data.find(
+        u => u.id === parseInt(formData.userId, 10)
+      ),
+      showCompleted: formData.showCompleted
+    });
   };
 
-  async fetchUsers() {
-    this.setState({ isLoadingUsers: true });
-    try {
-      const data = await Api.fetchUsers();
-      return this.setState({
-        users: data,
-        isLoadingUsers: false
-      });
-    } catch (error) {
-      return this.setState({ error, isLoadingUsers: false });
-    }
-  }
-
-  async fetchTodos() {
-    this.setState({
-      isLoadingTodos: true
-    });
-
-    const userId = this.state.selectedUser.id;
-    const showCompleted = this.state.showCompleted;
-    try {
-      const data = await Api.fetchTodosByUser(userId, showCompleted);
-      // if we've changed settings in the background, ignore this update
-      if (
-        userId === this.state.selectedUser.id &&
-        showCompleted === this.state.showCompleted
-      ) {
-        // set our state
-        this.setState({
-          todos: data,
-          isLoadingTodos: false,
-          todosUpdatedAt: new Date()
-        });
-      }
-    } catch (error) {
-      clearInterval(this.interval);
-      this.setState({ todosError: error, isLoadingTodos: false });
-    }
-  }
-
-  refreshUsers() {
-    this.fetchUsers();
-  }
-
-  componentDidMount() {
-    this.fetchUsers();
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  onFilterSubmit(formData) {
-    this.setState(
-      {
-        selectedUser: this.state.users.find(
-          u => u.id === parseInt(formData.userId, 10)
-        ),
-        showCompleted: formData.showCompleted,
-        todos: null,
-        todosError: null
-      },
-      () => {
-        this.fetchTodos();
-        clearInterval(this.interval);
-        this.interval = setInterval(() => {
-          this.fetchTodos();
-        }, 6000);
-      }
+  if (usersState.error) {
+    return (
+      <div>
+        <h3>There was an error loading users:</h3>
+        <p>{usersState.error.message}</p>
+      </div>
     );
   }
 
-  componentDidCatch(error, info) {
-    console.log({ error, info });
-    this.setState({ error });
-  }
-
-  render() {
-    const {
-      isLoadingUsers,
-      users,
-      selectedUser,
-      todosUpdatedAt,
-      isLoadingTodos,
-      todos,
-      error,
-      todosError
-    } = this.state;
-
-    if (error) {
-      return (
-        <div>
-          <h3>There was an error:</h3>
-          <p>{error.message}</p>
-        </div>
-      );
-    }
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="columns ">
-            <div className="column is-one-quarter">
-              <Filter
-                users={users}
-                isLoadingUsers={isLoadingUsers}
-                updateUserSettings={this.onFilterSubmit}
-                refreshUsers={this.refreshUsers}
-              />
-            </div>
-            <div className="column is-three-quarters">
-              <Todos
-                user={selectedUser}
-                updatedAt={todosUpdatedAt}
-                todos={todos}
-                isLoading={isLoadingTodos}
-                error={todosError}
-              />
-            </div>
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="columns ">
+          <div className="column is-one-quarter">
+            <Filter
+              users={usersState.data}
+              isLoadingUsers={usersState.isLoading}
+              updateUserSettings={onFilterSubmit}
+              refreshUsers={() => {
+                loadUsers();
+              }}
+            />
+          </div>
+          <div className="column is-three-quarters">
+            <Todos
+              user={filterData.selectedUser}
+              showCompleted={filterData.showCompleted}
+            />
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
+
+export default withErrorBoundary(App);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css';
+import { hijackEffects } from 'stop-runaway-react-effects';
+
+if (process.env.NODE_ENV !== 'production') {
+  hijackEffects();
+}
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
