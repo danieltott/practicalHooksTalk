983376b35e2ed1275161708ae7c79826ca1b85a4
Dan Ott <dan@dtott.com>
2019-05-24 15:52:17 -0400

    Revert "setFormData"
    This reverts commit b18062fdec41c487e315cd9d4a5978d204f9a4f3.

--- src/Filter.js
+++ src/Filter.js
@@ -1,111 +1,120 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SelectUsers from './SelectUsers';
-import { Heading, Pane, Button, Card, FormField, Radio } from 'evergreen-ui';
+import {
+  Heading,
+  Pane,
+  Button,
+  Card,
+  FormField,
+  RadioGroup,
+} from 'evergreen-ui';
 
-const Filter = ({
-  isLoadingUsers,
-  users,
-  updateUserSettings,
-  refreshUsers,
-}) => {
-  const [formData, setFormData] = useState({
+export default class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.updateFormInput = this.updateFormInput.bind(this);
+  }
+
+  state = {
     userId: '',
     showCompleted: 'All',
-  });
-  const [isValid, setIsValid] = useState(false);
+    isValid: false,
+  };
 
-  function updateFormInput(name, value) {
-    setFormData({
-      [name]: value,
+  updateFormInput(name, value) {
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => {
+        this.validate();
+      }
+    );
+  }
+
+  validate() {
+    this.setState({
+      isValid: !!(this.state.userId && this.state.showCompleted),
     });
-
-    validate();
   }
 
-  function validate() {
-    setIsValid(!!(formData.userId && formData.showCompleted));
-  }
+  render() {
+    const {
+      isLoadingUsers,
+      users,
+      updateUserSettings,
+      refreshUsers,
+    } = this.props;
 
-  return (
-    <Card
-      is="form"
-      border="default"
-      padding=".5em"
-      background="blueTint"
-      data-testid="filtersform"
-      onSubmit={e => {
-        e.preventDefault();
-        updateUserSettings(formData);
-      }}
-    >
-      <Heading
-        is="legend"
-        marginX="-.5em"
-        paddingX=".5em"
-        marginTop="-.5em"
-        paddingY=".5em"
-        marginBottom=".5em"
-        borderBottom="1px solid #E4E7EB"
-        size={300}
+    return (
+      <Card
+        is="form"
+        border="default"
+        padding=".5em"
+        background="blueTint"
+        data-testid="filtersform"
+        onSubmit={e => {
+          e.preventDefault();
+          updateUserSettings({
+            userId: this.state.userId,
+            showCompleted: this.state.showCompleted,
+          });
+        }}
       >
-        Filter Todos:
-      </Heading>
-      <FormField label="User:" labelFor="userId" paddingY="1em">
-        <SelectUsers
-          isLoading={isLoadingUsers}
-          users={users}
-          userId={formData.userId}
-          onChange={updateFormInput}
-        />
-      </FormField>
-      <Pane marginBottom="1em">
-        <Button
-          appearance="minimal"
-          onClick={refreshUsers}
-          type="button"
-          height={24}
-          paddingX={0}
+        <Heading
+          is="legend"
+          marginX="-.5em"
+          paddingX=".5em"
+          marginTop="-.5em"
+          paddingY=".5em"
+          marginBottom=".5em"
+          borderBottom="1px solid #E4E7EB"
+          size={300}
         >
-          Refresh Users...
-        </Button>
-      </Pane>
-      <FormField label="Show:">
-        <Pane role="group">
-          <Radio
-            name="showCompleted"
-            value="All"
-            label="All"
-            checked={formData.showCompleted === 'All'}
-            onChange={() => updateFormInput('showCompleted', 'All')}
+          Filter Todos:
+        </Heading>
+        <FormField label="User:" labelFor="userId" paddingY="1em">
+          <SelectUsers
+            isLoading={isLoadingUsers}
+            users={users}
+            userId={this.state.userId}
+            onChange={this.updateFormInput}
           />
-          <Radio
-            name="showCompleted"
-            value="Completed"
-            label="Completed"
-            checked={formData.showCompleted === 'Completed'}
-            onChange={() => updateFormInput('showCompleted', 'Completed')}
-          />
-          <Radio
-            name="showCompleted"
-            value="Not Completed"
-            label="Not Completed"
-            checked={formData.showCompleted === 'Not Completed'}
-            onChange={() => updateFormInput('showCompleted', 'Not Completed')}
-          />
+        </FormField>
+        <Pane marginBottom="1em">
+          <Button
+            appearance="minimal"
+            onClick={refreshUsers}
+            type="button"
+            height={24}
+            paddingX={0}
+          >
+            Refresh Users...
+          </Button>
         </Pane>
-      </FormField>
+        <FormField label="Show:">
+          <RadioGroup
+            value={this.state.showCompleted}
+            options={[
+              { label: 'All', value: 'All' },
+              { label: 'Completed', value: 'Completed' },
+              { label: 'Not Completed', value: 'Not Completed' },
+            ]}
+            onChange={value => this.updateFormInput('showCompleted', value)}
+          />
+        </FormField>
 
-      <Pane textAlign="right">
-        <Button
-          type="submit"
-          disabled={isLoadingUsers || !isValid}
-          appearance="primary"
-        >
-          Submit
-        </Button>
-      </Pane>
-    </Card>
-  );
-};
-
-export default Filter;
+        <Pane textAlign="right">
+          <Button
+            type="submit"
+            disabled={isLoadingUsers || !this.state.isValid}
+            appearance="primary"
+          >
+            Submit
+          </Button>
+        </Pane>
+      </Card>
+    );
+  }
+}
