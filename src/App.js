8aa89fa62e4d0c467cb9dbd24c9784780bca4e1c
Dan Ott <dan@dtott.com>
2019-05-28 09:52:33 -0400

    Create useLoad and integrate with App

--- src/App.js
+++ src/App.js
@@ -1,111 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Pane, majorScale, Alert, Text } from 'evergreen-ui';
 import Api from './Api';
 import Todos from './Todos';
 import Filter from './Filter';
+import useLoad from './useLoad';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+const withErrorBoundary = RenderedComponent =>
+  class extends React.Component {
+    state = {
+      globalError: null,
+    };
 
-    this.onFilterSubmit = this.onFilterSubmit.bind(this);
-    this.fetchUsers = this.fetchUsers.bind(this);
-    this.refreshUsers = this.refreshUsers.bind(this);
-  }
+    componentDidCatch(error, info) {
+      console.log({ error, info });
+      this.setState({ globalError: error });
+    }
 
-  state = {
-    // global error
-    globalError: null,
-    // fetching users state
-    users: null,
-    isLoadingUsers: false,
-    usersError: null,
-
-    // filter form state
-    selectedUser: null,
-    showCompleted: '',
+    render() {
+      return <RenderedComponent globalError={this.state.globalError} />;
+    }
   };
 
-  async fetchUsers() {
-    this.setState({ isLoadingUsers: true });
-    try {
-      const data = await Api.fetchUsers();
-      return this.setState({
-        users: data,
-        isLoadingUsers: false,
-      });
-    } catch (error) {
-      return this.setState({ usersError: error, isLoadingUsers: false });
-    }
+const App = ({ globalError }) => {
+  const [{ selectedUser, showCompleted }, setFilter] = useState({
+    selectedUser: null,
+    showCompleted: '',
+  });
+
+  const [usersState, loadUsers] = useLoad(Api.fetchUsers);
+
+  const {
+    data: users,
+    isLoading: isLoadingUsers,
+    error: usersError,
+  } = usersState;
+
+  function refreshUsers() {
+    loadUsers();
   }
 
-  refreshUsers() {
-    this.fetchUsers();
-  }
-
-  componentDidMount() {
-    this.fetchUsers();
-  }
-
-  onFilterSubmit(formData) {
-    this.setState({
-      selectedUser: this.state.users.find(
-        u => u.id === parseInt(formData.userId, 10)
-      ),
+  function onFilterSubmit(formData) {
+    setFilter({
+      selectedUser: users.find(u => u.id === parseInt(formData.userId, 10)),
       showCompleted: formData.showCompleted,
     });
   }
 
-  componentDidCatch(error, info) {
-    console.log({ error, info });
-    this.setState({ globalError: error });
-  }
-
-  render() {
-    const {
-      isLoadingUsers,
-      users,
-      globalError,
-      usersError,
-      selectedUser,
-      showCompleted,
-    } = this.state;
-
-    if (globalError || usersError) {
-      return (
-        <Pane
-          maxWidth={800}
-          border
-          marginX="auto"
-          marginY={majorScale(2)}
-          padding={majorScale(2)}
-        >
-          <Alert intent="danger" title="There was an error:">
-            {globalError && <Text>{globalError.message}</Text>}
-            {usersError && <Text>{usersError.message}</Text>}
-          </Alert>
-        </Pane>
-      );
-    }
+  if (globalError || usersError) {
     return (
       <Pane
         maxWidth={800}
+        border
         marginX="auto"
         marginY={majorScale(2)}
-        display="flex"
+        padding={majorScale(2)}
       >
-        <Pane flex={1} padding={majorScale(2)}>
-          <Filter
-            users={users}
-            isLoadingUsers={isLoadingUsers}
-            updateUserSettings={this.onFilterSubmit}
-            refreshUsers={this.refreshUsers}
-          />
-        </Pane>
-        <Pane flex={3} padding={majorScale(2)}>
-          <Todos user={selectedUser} showCompleted={showCompleted} />
-        </Pane>
+        <Alert intent="danger" title="There was an error:">
+          {globalError && <Text>{globalError.message}</Text>}
+          {usersError && <Text>{usersError.message}</Text>}
+        </Alert>
       </Pane>
     );
   }
-}
+  return (
+    <Pane maxWidth={800} marginX="auto" marginY={majorScale(2)} display="flex">
+      <Pane flex={1} padding={majorScale(2)}>
+        <Filter
+          users={users}
+          isLoadingUsers={isLoadingUsers}
+          updateUserSettings={onFilterSubmit}
+          refreshUsers={refreshUsers}
+        />
+      </Pane>
+      <Pane flex={3} padding={majorScale(2)}>
+        <Todos user={selectedUser} showCompleted={showCompleted} />
+      </Pane>
+    </Pane>
+  );
+};
+
+export default withErrorBoundary(App);
